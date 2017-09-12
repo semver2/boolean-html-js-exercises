5e5f5a7a49dc1db953ddf426499e9188f4051321
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2017-09-12 02:26:13 -0300

    @gpincheiraa adding sass and html templates

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
     entry: [
       "./index.js"
@@ -9,9 +12,22 @@ module.exports = {
     module: {
         rules: [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-            { test: /\.md$/, loaders: [ "html-loader", "markdown-loader"] }
+            { test: /\.md$/, loaders: [ "html-loader", "markdown-loader" ] },
+            { test: /\.scss$/,
+              use: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: ['css-loader', 'sass-loader']
+              })
+            }
         ]
     },
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+        new HtmlWebpackPlugin({
+          template: 'index.html',
+          inject: 'body'
+        })
+    ],
     devtool: "eval-source-map",
     devServer: {
         filename: "index.bundle.js",
