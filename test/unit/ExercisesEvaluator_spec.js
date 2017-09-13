4b9eef8b405f6725f3f054366ff8313c85b4565b
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2017-09-13 20:36:06 -0300

    @gpincheiraa Adding test for the initialize method in the ExercisesEvaluator class

--- test/unit/ExercisesEvaluator_spec.js
+++ test/unit/ExercisesEvaluator_spec.js
@@ -9,9 +9,38 @@ describe("ExercisesEvaluator Class", () => {
             innerHTML: ""
         })
         exercisesEvaluator = new ExercisesEvaluator()
+        global.document.getElementById.restore()
     })
 
     it("should be defined", () => {
         expect(exercisesEvaluator).toBeDefined()
+    })
+    it("should set the innerHTML in the right div given the template exercises in evaluator instance", () => {
+        const divs = [
+            {
+                selector: "exercise-1-statement",
+                innerHTML: ""
+            },
+            {
+                selector: "exercise-2-statement",
+                innerHTML: ""
+            }
+        ]
+        const templates = [
+            "<h1>Exercise1</h1>",
+            "<h2>Exercise2</h2>"
+        ]
+        const documentMock = sinon.stub(global.document, 'getElementById')
+
+        documentMock.withArgs(divs[0].selector).returns(divs[0])
+        documentMock.withArgs(divs[1].selector).returns(divs[1])
+
+        exercisesEvaluator.exercisesTemplates = templates;
+        exercisesEvaluator.initialize()
+
+        templates.forEach((template, index) => {
+            expect(divs[index].innerHTML).toEqual(template)
+        })
+        global.document.getElementById.restore()
     })
 })
